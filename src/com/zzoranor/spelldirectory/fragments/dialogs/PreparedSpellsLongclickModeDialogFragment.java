37da035d4b2537e42f877a87e635bc20355d8a28
kwatson <kwatson@nerdery.com>
2013-06-13 17:41:36 -0500

    Added appropriate navigation drawers in place of all options menus. Switched some things up to allow for dialog fragments in some place in place of submenus.

---
+++ src/com/zzoranor/spelldirectory/fragments/dialogs/PreparedSpellsLongclickModeDialogFragment.java
@@ -0,0 +1,11 @@
+package com.zzoranor.spelldirectory.fragments.dialogs;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: kwatson
+ * Date: 6/13/13
+ * Time: 2:19 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class PreparedSpellsLongclickModeDialogFragment {
+}
---
+++ src/com/zzoranor/spelldirectory/fragments/dialogs/SpellsLongclickModeDialogFragment.java
@@ -0,0 +1,11 @@
+package com.zzoranor.spelldirectory.fragments.dialogs;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: kwatson
+ * Date: 6/13/13
+ * Time: 4:52 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class SpellsLongclickModeDialogFragment {
+}
