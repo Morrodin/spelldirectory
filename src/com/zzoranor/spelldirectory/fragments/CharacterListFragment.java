ef8525516e9e7772262f2d088b6695252381dd10
Morrodin <whiplord@gmail.com>
2013-06-15 02:08:31 -0500

    Character list is now turned into a fragment. Added a shadow to menu drawer.

---
+++ src/com/zzoranor/spelldirectory/fragments/CharacterListFragment.java
@@ -0,0 +1,11 @@
+package com.zzoranor.spelldirectory.fragments;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Kenton
+ * Date: 6/14/13
+ * Time: 11:23 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class CharacterListFragment {
+}
